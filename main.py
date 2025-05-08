9c28f7e4fabd43f8cecb785f642d8eec9e8526ef
camel_assault <camels.personal@gmail.com>
2025-05-08 13:52:06 -0700

    BK 2: Removed hard coded book, pulling book file from CLI arguments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_word_count
 from stats import get_letter_count
 from stats import print_character_report
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as file:
@@ -8,7 +9,11 @@ def get_book_text(filepath):
         return file_string
     
 def main():
-    book_file = "books/frankenstein.txt"
+    arguments = sys.argv
+    if len(arguments) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_file = arguments[1]
     book_text = get_book_text(book_file)
     word_count = get_word_count(book_text)
     letter_count_dictionary = get_letter_count(book_text)
